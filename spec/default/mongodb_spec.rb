7990ae358539f182a77bee4d3ba912814ba0321b
bells17 <bells171@gmail.com>
2015-04-02 11:39:43 +0900

    add mongodb test

--- spec/default/mongodb_spec.rb
+++ spec/default/mongodb_spec.rb
@@ -8,3 +8,7 @@ describe service('mongod'), :if => os[:family] == 'redhat' do
   it { should be_enabled }
   it { should be_enabled.with_level(3) }
 end
+
+describe port(27017) do
+  it { should be_listening.on('0.0.0.0').with('tcp') }
+end
